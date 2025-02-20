9e6bc2422977b39df28ada9e9f23fca23bc9ad77
rashed mamdouh <rashedmamdouh699@gmail.com>
2025-02-20 17:52:45 +0200

    commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
-const port = 3000;
+import "dotenv/config";
+
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json()); //Parse the JSON
 
